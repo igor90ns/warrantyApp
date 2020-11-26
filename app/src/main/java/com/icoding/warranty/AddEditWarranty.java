93ff8b2d908719ea3e0fbfb16a4f124f5d716e19
igor90ns <aleksic90@gmail.com>
2020-11-26 20:31:45 +0100

    Camera Intent, file storage access and photo preview

--- app/src/main/java/com/icoding/warranty/AddEditWarranty.java
+++ app/src/main/java/com/icoding/warranty/AddEditWarranty.java
@@ -1,6 +1,10 @@
 package com.icoding.warranty;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,14 +14,23 @@ import com.icoding.warranty.data.WarrantyDataDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import io.reactivex.Scheduler;
@@ -37,6 +50,11 @@ public class AddEditWarranty extends AppCompatActivity {
     EditText descriptionInput;
     EditText dateInput;
     EditText durationInput;
+    ImageButton cameraButton;
+    ImageView imageView;
+    String currentPhotoPath;
+    File photoFile = null;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     @Override
@@ -50,6 +68,8 @@ public class AddEditWarranty extends AppCompatActivity {
         descriptionInput = findViewById(R.id.wDescription);
         dateInput = findViewById(R.id.wDate);
         durationInput = findViewById(R.id.wDuration);
+        cameraButton = findViewById(R.id.cameraButton);
+        imageView = findViewById(R.id.imageView);
 
         Intent intent = getIntent();
         final String warrantyIdString = intent.getAction();
@@ -108,6 +128,14 @@ public class AddEditWarranty extends AppCompatActivity {
             }
 
         });
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+
+            }
+        });
     }
 
     public void insertWarranty(ArrayList<WarrantyData> newWarranty){
@@ -132,7 +160,6 @@ public class AddEditWarranty extends AppCompatActivity {
         compositeDisposable = mWarrantyDataDatabase.getWarrantyDataDao().getSingleWarrenty(warrantyId).subscribe(new Consumer<List<WarrantyData>>() {
             @Override
             public void accept(List<WarrantyData> warrantyData) throws Exception {
-                Log.e("TAG", "vratila se " + warrantyData.get(0).name);
                 handleDatabaseQuery(warrantyData);
             }
         });
@@ -156,4 +183,93 @@ public class AddEditWarranty extends AppCompatActivity {
         });
         finish();
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.icoding.warranty.fileprovider",
+                        photoFile);
+                System.out.println("Uri" + photoURI.toString());
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+                    setPic();
+
+                    //full size
+//                File imgFile = new  File(currentPhotoPath);
+//                System.out.println("putanja je " + currentPhotoPath);
+//                if(imgFile.exists()) {
+//                    System.out.println("Ima fajla");
+//                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+//                    imageView.setImageBitmap(myBitmap);
+//                }
+        }
+    }
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        imageView.setImageBitmap(bitmap);
+    }
+
+
 }
